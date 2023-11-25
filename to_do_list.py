379ef23804468e32428031c62b261ce30e6e9ef7
RoshanBichuka <roshanbichuka@gmail.com>
2023-11-25 17:48:55 +0530

    colour updated

--- to_do_list.py
+++ to_do_list.py
@@ -40,7 +40,7 @@ window.title("TO_DO_LIST")
 frame_task = Frame(window)
 frame_task.pack()
 
-listbox_task = Listbox(frame_task, bg="white", fg="black", height=15, width=50, font="Century")
+listbox_task = Listbox(frame_task, bg="lightgrey", fg="black", height=15, width=50, font="Century")
 listbox_task.pack(side=tkinter.LEFT)
 
 scrollbar_task = Scrollbar(frame_task)
